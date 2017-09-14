8210447dc3401575ca79c2dfecc204fe0339c2af
Ezequiel <ezequiel1de1oliveira@gmail.com>
2017-09-14 20:03:56 -0300

    criação e implementaçao da classe Blinker

---
+++ inc/blinker.hpp
@@ -0,0 +1,23 @@
+#ifndef BLINKER_HPP
+#define BLINKER_HPP
+#include "formagenerica.hpp"
+
+class Blinker : public FormaGenerica {
+
+private:
+
+   char ** Matriz;
+
+ public:
+
+    Blinker();
+   ~Blinker();
+
+
+
+
+
+};
+
+
+#endif
---
+++ src/blinker.cpp
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <iostream>
+#include "formagenerica.hpp"
+#include "blinker.hpp"
+
+
+
+using namespace std;
+
+Blinker::Blinker(){
+int i;
+int j;
+setNumLinha(3);
+setNumColuna(3);
+
+Matriz = (char**)malloc(getNumLinha()*sizeof(sizeof(char*)));
+
+for(i = 0; i < getNumLinha() ; i++){
+
+Matriz[i] = (char*)malloc(getNumColuna()*sizeof(char));
+
+}
+
+for(i = 0 ; i < getNumLinha(); i++){
+
+for(j = 0 ; j < getNumColuna(); j++){
+
+Matriz[i][j] = '-';
+
+}
+
+}
+
+Matriz[0][1] = '+';
+Matriz[1][1] = '+';
+Matriz[2][1] = '+';
+
+
+}
+
+Blinker::~Blinker(){
+
+free(Matriz);
+Matriz = NULL;
+
+}
