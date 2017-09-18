8057583b64d22a08bb62a07708d6f02238b860b9
Ezequiel <ezequiel1de1oliveira@gmail.com>
2017-09-18 20:53:04 -0300

    em fase de finalização

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
 #include "grade.hpp"
 #include "block.hpp"
@@ -26,12 +27,18 @@ cout << campoPadrao.getNumColuna();
 
 cout << " Bem vindo(a) ao Conway's Game of Life" << endl;
 cout << endl;
+cout << endl;
 cout << "O jogo se baseia em quatro regras" << endl;
+cout << endl;
 cout << "São elas: " << endl;
 cout << "1 - Qualquer célula viva com menos de dois vizinhos vivos morre de solidão." << endl;
+cout << endl;
 cout << "2 - Qualquer célula viva com mais de três vizinhos vivos morre de superpopulação." << endl;
+cout << endl;
 cout << "3 - Qualquer célula morta com exatamente três vizinhos vivos se torna uma célula viva." << endl;
+cout << endl;
 cout << "4 - Qualquer célula viva com dois ou três vizinhos vivos continua no mesmo estado para a próxima geração." << endl;
+cout << endl;
 cout << endl;
 cout << "digite \"1\" para prosseguir: ";
 cin >> BotaoContinua;
@@ -48,13 +55,32 @@ system("clear||cls");
 
 cout << "Instruções: " << endl;
 cout << endl;
+cout << endl;
 cout << "1 - Primeiramente Deve-se escolher o tamanho do campo ou escolher o tamanho padrão." << endl;
-cout << "2 - Logo após deve-se decidir o Modo de jogo, Ver a Gosper glider gun ou Campo livre. " << endl;
-cout << "3 - Gosper glider gun: será exibido a gun." << endl;
-cout << "4 - Modo Livre: O campo ficará livre para escolher as posições de suas células vivas e iniciar o jogo." << endl;
-cout << "5 - O campo padrão e composto por 40 linhas e 100 colunas." << endl;
-cout << "6 - No caso de Campo Personalizado no modo Glider gun o campo deve ter no minimo 35 linhas e 35 colunas." << endl;
-cout << "7 - Durante a execução o jogo irá perguntar se deseja continuar ou parar." << endl;
+cout << endl;
+cout << "2 - Se a escolha for o campo Personalizavel, será perguntado as medidas." << endl;
+cout << endl;
+cout << "3 - Logo após deve-se decidir o Modo de jogo, Ver a Gosper glider gun ou Campo livre. " << endl;
+cout << endl;
+cout << "4 - Gosper glider gun: será exibido a gun." << endl;
+cout << endl;
+cout << "5 - Modo Livre: O campo ficará livre para escolher as posições de suas células vivas e iniciar o jogo." << endl;
+cout << endl;
+cout << "6 - No modo livre pode-se dar vida a quantas células desejar, se estiver dentro dos parâmetros do campo. " << endl;
+cout << endl;
+cout << "7 - O campo padrão e composto por 40 linhas e 100 colunas." << endl;
+cout << endl;
+cout << "8 - No caso de Campo Personalizado no modo Glider gun o campo deve ter no minimo 40 linhas e 40 colunas." << endl;
+cout << endl;
+cout << "9 - O campo terá um total de 300 geraçoes seguidas" << endl;
+cout << endl;
+cout << "10- Chegando na geração 300 aperte \"0\" para parar, ou \"1\" para mais 300 Geraçoes." << endl;
+cout << endl;
+cout << "11- Ao fim o jogo e finalizado." << endl;
+cout << endl;
+cout << endl;
+cout <<" Bom Jogo !!" << endl;
+cout << endl;
 cout << endl;
 cout << "digite \"1\" para prosseguir: ";
 cin >> BotaoContinua;
@@ -74,17 +100,21 @@ cout << "escolha de tamanho do campo!" << endl;
 cout << endl;
 cout << "Escolha: " << endl;
 cout << "1 - Tamanho de campo padrão" << endl;
+cout << endl;
 cout << "2 - Tamanho de campo Personalizado" << endl;
+cout << endl;
 cout << "Digite a opção desejada: " ;
 cin >> TamanhoMatriz;
  if(TamanhoMatriz == 2){
 
 cout << "Digite as dimensões do seu campo!" << endl;
+cout << endl;
 cout << "Numéro de linhas: " << endl;
 cin >> NumLinha;
 cout << endl;
 cout << "Numéro de colunas: " << endl;
 cin >> NumColuna;
+cout << endl;
 
 }
 
@@ -93,8 +123,11 @@ system("clear||cls");
 cout << "Modo de jogo" << endl;
 cout << endl;
 cout << "Digite:" << endl;
+cout << endl;
 cout << "1 - Gosper glider gun" << endl;
+cout << endl;
 cout << "2 - Modo livre" << endl;
+cout << endl;
 cout << "Modo: ";
 cin >> ModoDeJogo;
 
@@ -113,8 +146,8 @@ if(ModoDeJogo == 1){
 
 if(TamanhoMatriz == 1){
 
-campoPadrao.RecebeBlockCoordenada(8,2);
 
+  campoPadrao.RecebeBlockCoordenada(8,2);
 campoPadrao.RecebeBlinkerCoordenada(8,12);
 
 campoPadrao.ReviveCelulaCoordenada(6,12);
@@ -162,7 +195,7 @@ campoPadrao.ReviveCelulaCoordenada(8,25);
 
 campoPadrao.ReviveCelulaCoordenada(9,25);
 
-campoPadrao.RecebeBlockCoordenada(6,34);
+campoPadrao.RecebeBlockCoordenada(6,36);
 
 campoPadrao.ImprimeMatriz();
 
@@ -180,25 +213,26 @@ while(BotaoContinua != 1){
 
 system("clear||cls");
 
-while(Parada != 0)
-{
-
-  while(Geracao < 300){
+do{
 
   campoPadrao.CicloDeVida();
 
     Geracao++;
-  }
+
+if(Geracao == 300){
 
 cout << "Para parar digite \"0\" para Continuar digite \"1\": " ;
 cin >> Parada;
+Geracao = 0;
+
 }
 
+}while(Parada);
 
 }else{
 
 
-  if(NumLinha >= 35 && NumColuna >= 35){
+  if(NumLinha >= 40 && NumColuna >= 40){
 
   Grade campoVariavel(NumLinha,NumColuna);
 
@@ -250,7 +284,7 @@ campoVariavel.RecebeBlinkerCoordenada(6,23);
 
   campoVariavel.ReviveCelulaCoordenada(9,25);
 
-  campoVariavel.RecebeBlockCoordenada(6,34);
+  campoVariavel.RecebeBlockCoordenada(6,36);
 
   campoVariavel.ImprimeMatriz();
 
@@ -267,23 +301,27 @@ campoVariavel.RecebeBlinkerCoordenada(6,23);
 
   system("clear||cls");
 
-  while(Parada != 0)
-  {
-
-    while(Geracao < 300){
+  do{
 
     campoVariavel.CicloDeVida();
 
       Geracao++;
-    }
+
+if(Geracao == 300){
 
   cout << "Para parar digite \"0\" para Continuar digite \"1\": " ;
   cin >> Parada;
-  }
+  Geracao = 0;
+
+}
+
+}while(Parada);
 
 }else{
 
 cout << "Campo requer Espaço para a Gosper glider gun" << endl;
+
+sleep(3);
 
 }
 
@@ -295,19 +333,22 @@ if(TamanhoMatriz == 1){
 
 campoPadrao.ReviveCelula();
 
-while(Parada != 0)
-{
 
-  while(Geracao < 300){
+do{
 
   campoPadrao.CicloDeVida();
 
     Geracao++;
-  }
+
+if(Geracao == 300){
 
 cout << "Para parar digite \"0\" para Continuar digite \"1\": " ;
 cin >> Parada;
+Geracao = 0;
+
 }
+
+}while(Parada);
 
 }else if(TamanhoMatriz == 2){
 
@@ -315,27 +356,32 @@ cin >> Parada;
 
   campoVariavel.ReviveCelula();
 
-  while(Parada != 0)
-  {
-
-    while(Geracao < 300){
+  do{
 
     campoVariavel.CicloDeVida();
 
       Geracao++;
-    }
+
+if(Geracao == 300){
 
   cout << "Para parar digite \"0\" para Continuar digite \"1\": " ;
   cin >> Parada;
-  }
+  Geracao = 0;
 
+}
+
+}while(Parada);
+
+}
 
 }
 
 
+system("clear||cls");
 
-
-}
+cout << "FIM DE JOGO!!" << endl;
+cout << ":)" << endl;
+cout << endl;
 
 return 0;
 }
